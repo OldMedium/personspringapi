5218e71184d4965ca31cb12699d4e8d4c97933a4
OldMedium <kluzinzki@gmail.com>
2021-09-03 22:57:59 -0300

    Created README.md file

---
+++ README.md
@@ -0,0 +1,26 @@
+## Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot
+
+### API Rest para gerenciamento de pessoas criada com Spring Boot.
+
+### Tecnologias Utilizadas:
+* Java 11
+* Maven 3.8.1
+* Spring Boot configurado com: Web, Data JPA, Actuator, H2
+* GitHub
+* Postman (para testes locais)
+* Heroku
+
+#### Para executar o projeto no terminal, digite o seguinte comando:
+
+```
+mvn spring-boot:run 
+```
+#### Após executar o comando acima, basta apenas abrir o seguinte endereço e visualizar a execução do projeto:
+
+```
+http://localhost:8080/api/v1/person
+```
+#### Para acessar o banco de dados H2, utilize o seguinte endereço:
+```
+http://localhost:8080/h2-console
+```
--- src/main/java/dio/springboot/personspringapi/controller/PersonController.java
+++ src/main/java/dio/springboot/personspringapi/controller/PersonController.java
@@ -6,6 +6,7 @@ import dio.springboot.personspringapi.entity.Person;
 import dio.springboot.personspringapi.exception.PersonNotFoundException;
 import dio.springboot.personspringapi.repository.PersonRepository;
 import dio.springboot.personspringapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/person")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
